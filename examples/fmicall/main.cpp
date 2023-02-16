35a168c7dce362028c14f67af5563387e2cfb411
Henrik Ejersbo <hejersbo@ece.au.dk>
2023-02-16 08:05:00 +0100

    Update example

--- examples/fmicall/main.cpp
+++ examples/fmicall/main.cpp
@@ -18,13 +18,21 @@ int baz(int p) {
     return p/2;
 }
 
-struct func_tbl_t {
+typedef struct _func_tbl_t {
     func_t f1;
     func_t f2;
     func_t f3;
-} func_tbl;
+} func_tbl_t;
+
+/* func_tbl_t func_tbl; */
+
+/* typedef struct _instance_t { */
+/*     func_tbl_t *ft; */
+/* } instance_t; */
 
 int main(int argc, char *argv[]) {
+
+    func_tbl_t func_tbl;
     func_tbl.f1 = &foo;
     func_tbl.f2 = &bar;
     func_tbl.f3 = &baz;
@@ -33,6 +41,13 @@ int main(int argc, char *argv[]) {
     func_tbl.f2(2);
     func_tbl.f3(3);
 
+    /* func_t f1 = &foo; */
+    /* f1(1); */
+    /* func_t f2 = &bar; */
+    /* f2(2); */
+    /* func_t f3 = &baz; */
+    /* f3(3); */
+
     /* func_t f = func_tbl.f1; */
     /* f(1); */
     /* f = func_tbl.f2; */
@@ -40,12 +55,14 @@ int main(int argc, char *argv[]) {
     /* f = func_tbl.f3; */
     /* f(3); */
 
-    func_t f = &foo;
-    f(1);
-    f = &bar;
-    f(2);
-    f = &baz;
-    f(3);
+    /* instance_t inst; */
+    /* inst.ft = (func_tbl_t*)malloc(sizeof(func_tbl_t)); */
+    /* inst.ft->f1 = &foo; */
+    /* inst.ft->f2 = &bar; */
+    /* inst.ft->f3 = &baz; */
+    /* inst.ft->f1(1); */
+    /* inst.ft->f2(2); */
+    /* inst.ft->f3(3); */
 
     /* func_t ar[3] = {foo, bar, baz}; */
     /* for (int i = 0; i < 3; i++) { */
